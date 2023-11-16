b746dc100214fe0588ed9d799bfe908702d7efaa
Vinay Pant <vinaypant24@gmail.com>
2023-11-16 16:22:13 +0530

    Code / Files Skeleton

---
+++ Controller.py
@@ -0,0 +1,21 @@
+import tkinter as tk 
+from tkinter import ttk 
+import customtkinter as ctk
+
+
+
+# Importing Program made files  : 
+from Model import database
+from Model import functions
+
+from View import main_page
+
+
+
+def main_page(): 
+    pass
+
+
+
+if __name__ == '__main__':
+    main_page()
---
+++ View/main_page.py
@@ -0,0 +1,10 @@
+import tkinter as tk 
+
+
+
+
+
+class Main_Page():
+
+    def __init__(self) -> None:
+        pass
