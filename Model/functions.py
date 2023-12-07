61f352d5284a5b060d9467601f466ecf050c030f
Vinay Pant <vinaypant24@gmail.com>
2023-12-07 20:14:10 +0530

    initial commit

--- Model/functions.py
+++
@@ -1,9 +0,0 @@
-from pubsub import pub
-
-planned_task_list   = []
-current_task_list  = []
-completed_task_list = []
-
-
-def add_planned_task(data):
-    planned_task_list.append(data)
--- View/fonts.py
+++
@@ -1,4 +0,0 @@
-super_small = ('Arial' ,8)
-super_small_bold  = ('Arial' , 9 , 'bold')
-
-clock_tick_bold   = ('Arial' , 18 , 'bold')
