ea2fed2fa82a87802d2208aee04b35a3492d1304
Vinay Pant <vinaypant24@gmail.com>
2023-12-10 21:04:15 +0530

    tk controls added app intitated differently

--- views_main_page.py
+++ views_main_page.py
@@ -22,27 +22,47 @@ class Main_Page():
 
 
         # Importing the images for the main controls  : 
-        self.dashboard_iamge  = ImageTk.PhotoImage(Image.open(r"app_assets\home_white.png"))
+        self.dashboard_image = ImageTk.PhotoImage(Image.open(r"app_assets\home_white.png"))
         self.avatar_image = ImageTk.PhotoImage(Image.open(r"app_assets\avatar.png"))
 
 
-        self.sidebar = tk.Frame(self.main_page , width=150 , background=colors.sidebar)
+        self.sidebar = tk.Frame(self.main_page , width=160 , background=colors.sidebar)
+        self.sidebar.pack_propagate(0)
 
         # Image and the username : Frame , Image, username Label : 
         self.user_frame = tk.Frame(self.sidebar , width=150 , height=100  , background=colors.sidebar)
         self.user_image  = tk.Label(self.user_frame , image=self.avatar_image , background=colors.sidebar)
-        self.user_name   = tk.Label(self.user_frame , text="Vinay Pant" , background=colors.sidebar)
+        self.user_name   = tk.Label(self.user_frame , text="Vinay Pant" , background=colors.sidebar , foreground=colors.white_color)
+
+        # golden seperator in the form of frame  : 
+        self.first_seperator  = tk.Frame(self.sidebar ,height=1 , background=colors.golden_color)
+
+        # frames for teh images and the text for the names  : 
+        self.dashboard_frame  =tk.Frame(self.sidebar , background=colors.sidebar)
+        self.dashboard_image_label = tk.Label(self.dashboard_frame , image=self.dashboard_image , background=colors.sidebar)
+        self.dashboard_image_text = tk.Label(self.dashboard_frame , text="Dashboard" , background=colors.sidebar , foreground=colors.white_color)
 
 
 
+
+        self.bottom_frame = tk.Frame(self.sidebar , background=colors.golden_color , height=30)
 
 
         # packing the controls : 
         self.sidebar.pack(side="left" , fill="y")
-        self.user_frame.pack(side="top")
+        self.user_frame.pack(side="top" , pady=15)
 
-        self.user_image.grid(row=0 , column=0)
-        self.user_name.grid(row = 0 , column=1)
+        self.user_image.grid(row=0 , column=0 , padx=10)
+        self.user_name.grid(row = 0 , column=1 , padx=10)
+
+        self.first_seperator.pack(side="top" , fill="x" , pady=10)
+
+        self.dashboard_frame.pack()
+        self.dashboard_image_label.grid(row = 0 , column=0)
+        self.dashboard_image_text.grid(row=0 , column=1)
+
+
+        self.bottom_frame.pack(side="bottom" , fill="x")
         self.main_page.mainloop()
 
 
