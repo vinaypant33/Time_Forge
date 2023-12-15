73841018d71cc0af79d0e583d75539edd00ee453
Vinay Pant <vinaypant24@gmail.com>
2023-12-16 04:27:42 +0530

    center screen spawning - main page

--- views_main_page.py
+++ views_main_page.py
@@ -14,10 +14,11 @@ class Main_Page():
         # Title for the main app : 
         self.mainpage.title("Time Forge")
 
-        # Setting up the geometry for the main app and setting the location to the center of the app  : 
-        self.x_location  = self.mainpage.winfo_screenwidth() // 2 + 450
-        self.y_location  = self.mainpage.winfo_screenheight() // 2 + 300
-        self.mainpage.geometry("900x600+{self.x_location}+{self.y_location}")
+        # Setting up the geometry for the main app and setting the loca(tion to the center of the app  : 
+        self.x_location  = (self.mainpage.winfo_screenwidth() //2) - (450)
+        self.y_location  = (self.mainpage.winfo_screenheight() //2) - (300)
+        print("x Location " + str(self.x_location) + "y Location" + str(self.y_location))
+        self.mainpage.geometry(f"900x600+{self.x_location}+{self.y_location}")
         
 
 
