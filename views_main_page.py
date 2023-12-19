b132b416487da66c8ba27614a23624af50d0d2ab
Vinay Pant <vinaypant24@gmail.com>
2023-12-19 10:21:00 +0530

    Icons Added

--- views_main_page.py
+++ views_main_page.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from tkinter import ttk
 import customtkinter as ctk
 import ttkbootstrap as btk
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+
+
 
 import colors
 
@@ -20,7 +24,16 @@ class Main_Page():
         print("x Location " + str(self.x_location) + "y Location" + str(self.y_location))
         self.mainpage.geometry(f"900x600+{self.x_location}+{self.y_location}")
         
+        # Setting up the theme for the main app  : will make this change later in settings page : 
+        self.mainpage._set_appearance_mode("dark")
 
+        # Defining Icon and make the main icon for the app  : 
+        # self.icon_image   = ImageTk.PhotoImage(Image.open(r"app_assets\main_icon.png"))
+        # self.icon_image = PhotoImage( file="app_assets\main_icon.png")
+
+        self.mainpage.after(201, lambda :self.mainpage.iconbitmap('app_assets\main_icon.ico'))
+
+        # self.mainpage.iconphoto(False , self.icon_image)
 
         # Calling the main app : 
         self.mainpage.mainloop()
